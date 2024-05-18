c6cff438ebd362f64af48f3eb9321c9544320624
alaska <vacumsio@gmail.com>
2024-05-18 21:52:43 +0300

    added allure
    need debug

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,26 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   retries: 2,
-  reporter: [['html', { outputFolder: 'tests/report', open: 'never' }]],
+  reporter: [['html', { outputFolder: 'tests/report', open: 'never' }]], //,[["line"], ["allure-playwright"]]
+  /*Error: Cannot find module 'line allure-playwright'
+Require stack:
+- D:\git\playwright_ts\node_modules\playwright\lib\program.js
+- D:\git\playwright_ts\node_modules\@playwright\test\cli.js
+    at Module._resolveFilename (node:internal/modules/cjs/loader:1145:15)
+    at Function.resolve (node:internal/modules/helpers:190:19)
+    at resolveReporter (D:\git\playwright_ts\node_modules\playwright\lib\program.js:321:18)
+    at D:\git\playwright_ts\node_modules\playwright\lib\program.js:315:40
+    at Array.map (<anonymous>)
+    at resolveReporterOption (D:\git\playwright_ts\node_modules\playwright\lib\program.js:315:30)
+    at overridesFromOptions (D:\git\playwright_ts\node_modules\playwright\lib\program.js:274:15)
+    at runTests (D:\git\playwright_ts\node_modules\playwright\lib\program.js:171:24)
+    at async t.<anonymous> (D:\git\playwright_ts\node_modules\playwright\lib\program.js:54:7) {
+  code: 'MODULE_NOT_FOUND',
+  requireStack: [
+    'D:\\git\\playwright_ts\\node_modules\\playwright\\lib\\program.js',
+    'D:\\git\\playwright_ts\\node_modules\\@playwright\\test\\cli.js'
+  ]
+}*/
   use: {
     launchOptions: {
       timeout: 0,
